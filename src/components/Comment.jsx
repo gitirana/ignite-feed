0784b6e6dea86bcf8ceb579ca50e99e43827d64b
gitirana <thaygitirana@gmail.com>
2023-06-06 20:28:42 -0300

    creating comment list structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from "./Comment.module.css";
+import { Trash, ThumbsUp } from "phosphor-react";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gitirana.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Thayná Gitirana</strong>
+              <time title="11 de maio às 8:13" dateTime="2022-05-11 08:00:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
