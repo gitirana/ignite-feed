c2972dd3d170a45b0d61d68046be5369e1d2670b
gitirana <thaygitirana@gmail.com>
2023-06-15 16:18:22 -0300

    create Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
@@ -7,10 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/gitirana.png"
-          />
+          <Avatar src="https://github.com/gitirana.png" />
 
           <div className={styles.authorInfo}>
             <strong>Thayná Gitirana</strong>
